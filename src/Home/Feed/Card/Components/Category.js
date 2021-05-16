7c33c8ed58aae9f7ef8abdc85ff1ba2a3a468430
DylanDrechsel <DrechselDylan@gmail.com>
2021-05-16 13:22:52 -0400

    Completed Category Component

---
+++ src/Home/Feed/Card/Components/Category.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = ({ category }) => {
+    return (
+        <div className="CardCaregory">
+            <b>{category}</b>
+        </div>
+    );
+};
+
+export default Category;
