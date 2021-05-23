d201cf054983bf3e89645dafd0a304bbb46e31a2
DylanDrechsel <DrechselDylan@gmail.com>
2021-05-23 14:03:53 -0400

    added global state that tracks website page for css changes to take effect

--- src/Home/Feed/Card/Components/Category.js
+++ src/Home/Feed/Card/Components/Category.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Category = ({ category }) => {
-    console.log(category)
     return (
 
         <div className="CardCaregory PostDetailCardCategory">
--- src/PostDetailPage/PostDetailPage.js
+++ src/PostDetailPage/PostDetailPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import { userIdState, tokenState, userNameState } from '../App';
+import { userIdState, tokenState, userNameState, websiteState } from '../App';
 import { useRecoilState } from 'recoil';
 import './PostDetailPage.css';
 import { Row, Col, Container } from 'react-bootstrap';
@@ -12,7 +12,12 @@ const PostDetailPage = ({ match }) => {
     const [userName] = useRecoilState(userNameState);
     const [data, setData] = useState({})
     const [haveData, setHaveData] = useState(false)
+    const [website, setWebsite] = useRecoilState(websiteState)
     const id = match.params.id
+
+    useEffect(() => {
+        setWebsite('postDetails')
+    }, [])
 
     // useEffect(() => {
     //     axios.get(`http://localhost:4000/posts/${id}`, {
