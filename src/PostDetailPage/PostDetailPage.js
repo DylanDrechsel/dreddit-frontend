48a21db63518e0d09533f7e565bedeb4cde5066e
DylanDrechsel <DrechselDylan@gmail.com>
2021-05-23 11:52:26 -0400

    set up PostDetailPage and PostDetailFeed

--- src/PostDetailPage/PostDetailPage.js
+++ src/PostDetailPage/PostDetailPage.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { userIdState, tokenState, userNameState } from '../App';
 import { useRecoilState } from 'recoil';
+import './PostDetailPage.css';
+import PostDetailFeed from './PostDetailFeed/PostDetailFeed'
 
 const PostDetailPage = ({ match }) => {
     const [userId] = useRecoilState(userIdState);
     const [token] = useRecoilState(tokenState);
     const [userName] = useRecoilState(userNameState);
+    const [data, setData] = useState({})
+    const [haveData, setHaveData] = useState(false)
     const id = match.params.id
 
-    axios.get(`http://localhost:4000/posts/${id}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-    .then(({ data }) => {
-        console.log(data)
-    })
+    useEffect(() => {
+        axios.get(`http://localhost:4000/posts/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(({ data }) => {
+            setData(data)
+            setHaveData(true)
+        })
+    }, [])
 
-    return (
+    if (haveData) {
+        console.log(data)
+    }
+
+    if (haveData) {
+        return (
         <div className="PostDetailPage">
-            TEST FROM POST DETAIL PAGE
+            <PostDetailFeed data={data}/>
         </div>
-    );
+        );
+    } else {
+        return (
+            <div>
+                LOADING
+            </div>
+        )
+    }
 };
 
 export default PostDetailPage;
