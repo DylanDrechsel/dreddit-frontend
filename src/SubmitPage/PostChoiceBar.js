8b4b2aa7bc3538a338120985f78b340e1c505659
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-14 15:13:25 -0400

    Completed function to handle button click

--- src/SubmitPage/PostChoiceBar.js
+++ src/SubmitPage/PostChoiceBar.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const PostChoiceBar = () => {
-    const [postState, setPostState] = useState('post')
+    const [postState, setPostState] = useState()
+
+    const handlePostNoImageClick = (event) => {
+        setPostState('post')
+    }
 
     return (
 			<div className='PostChoiceBar'>
-				<Container>
+				<Container onClick={handlePostNoImageClick}>
 					<Row className={postState === 'post' ? 'PostNoImageClicked' : 'PostNoImage'}>
                         <Col>
 						    <h4 className='PostNoImageText' style={{ textAlign: 'center'}}>Post</h4>
