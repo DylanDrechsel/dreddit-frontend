023cb2bc37ac1656df5b4eb3603d8605fed71715
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-14 14:43:36 -0400

    added hover and active effects for post with no image button

--- src/SubmitPage/PostChoiceBar.js
+++ src/SubmitPage/PostChoiceBar.js
@@ -3,14 +3,14 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const PostChoiceBar = () => {
     return (
-        <div className='PostChoiceBar'>
-            <Container>
-                <Row className='PostNoImage'>
-                    <h4>Post</h4>
-                </Row>
-            </Container>
-        </div>
-    );
+			<div className='PostChoiceBar'>
+				<Container>
+					<Row className='PostNoImage'>
+						<h4 className='PostNoImageText' style={{ textAlign: 'center'}}>Post</h4>
+					</Row>
+				</Container>
+			</div>
+		);
 };
 
 export default PostChoiceBar;
