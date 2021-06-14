882e495489e68333750edb0a5906453b06f36864
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-14 14:31:01 -0400

    Started the post button for post submit without image

--- src/SubmitPage/PostChoiceBar.js
+++ src/SubmitPage/PostChoiceBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const PostChoiceBar = () => {
     return (
-        <div>
-            
+        <div className='PostChoiceBar'>
+            <Container>
+                <Row className='PostNoImage'>
+                    <h4>Post</h4>
+                </Row>
+            </Container>
         </div>
     );
 };
