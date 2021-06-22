ff761b43636958f15cf5834fecfc5bab013e0382
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-22 14:37:55 -0400

    Added website states for post functions. Also tested all the features I added again

--- src/SubmitPage/PostBoxes/Components/Post.js
+++ src/SubmitPage/PostBoxes/Components/Post.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { tokenState, websiteState } from '../../../App';
+import { useRecoilState } from 'recoil';
 import { Button } from 'react-bootstrap';
 
 const Post = ({ post, imagePost }) => {
+    const [website] = useRecoilState(websiteState);
+
+    console.log(website)
+
     return (
         <div className="PostButtonDiv">
             <Button
 					className='PostButton'
 					variant='outline'
-                    onClick={imagePost}>
+                    onClick={website === 'submit' ? post : imagePost}>
 					POST
 			</Button>
         </div>
