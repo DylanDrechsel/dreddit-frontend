51c0efba2d3db030eee66352e6fd596c8843c8d2
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-22 14:22:29 -0400

    Cleaned up ImagePost.js... Next step is to fixed the saveDraft function and add a state tracker to the submit route

--- src/SubmitPage/PostBoxes/ImagePost.js
+++ src/SubmitPage/PostBoxes/ImagePost.js
@@ -11,10 +11,7 @@ import PostButton from './Components/Post';
 const ImagePost = () => {
     const [token] = useRecoilState(tokenState);
     const [imagePostData, setImagePostData] = useState({})
-    const [image, setImage] = useState({})
     let formData = new FormData()
-    console.log(imagePostData)
-    console.log(image)
 
     const handlePostDataInput = (event) => {
         const input = { ...imagePostData }
@@ -22,18 +19,26 @@ const ImagePost = () => {
         setImagePostData(input)
     }
 
-    const post = () => {
+    const imagePost = (event) => {
+        event.preventDefault()
+
+        const myForm = document.getElementById("imageForm")
+        formData = new FormData(myForm)
+
+        formData.append('title', imagePostData.title)
+        formData.append('category', imagePostData.category);
+        formData.append('published', 'true');
+
         axios({
             url: 'http://localhost:4000/posts/create/image',
             method: 'POST',
-            data: {...imagePostData, published: true},
-            'content-type':  'multipart/form-data',
+            data: formData,
             headers: {
-                Authorization: `Bearer ${token}`
+                Authorization: `Bearer ${token}`,
             }
         })
-        .then (({ data }) => {
-            console.log(data)
+        .then (res => {
+            console.log(res)
         })
     }
 
@@ -48,125 +53,7 @@ const ImagePost = () => {
         })
     }
 
-    const handleImageUpload = (event) => {
-        event.preventDefault()
-        let image = event.target.files[0]
-        console.log(image)
-        console.log(event.target.value)
-
-        formData.append('image', event.target.files[0])
-
-
-        console.log(formData)
-
-        setImage({
-            image,
-            path: event.target.value,
-            size: event.target.files[0].size,
-            originalname: event.target.files[0].name
-        })
-
-
-        // setImagePostData({
-		// 			...imagePostData,
-        //             image: image
-					
-		// 		});
-    }
-
-  /*   {image: File}
-image: File
-lastModified: 1607734472265
-lastModifiedDate: Fri Dec 11 2020 19:54:32 GMT-0500 (Eastern Standard Time) {}
-name: "apbnvkrv94461.jpg"
-size: 338955
-type: "image/jpeg"
-webkitRelativePath: ""
-__proto__: File
-__proto__: Object */
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-        // let formData = new FormData()
-
-        console.log(imagePostData.image)
-
-        // formData.append('image', image)
-
-
-        axios({
-            url: 'http://localhost:4000/posts/create/single/image',
-            method: 'POST',
-            // enctype: 'multipart/form-data',
-            data: {
-                // title: imagePostData.title,
-                // category: imagePostData.category,
-                // image: {
-                    path: `image/${image.originalname}`,
-                    mimetype:  "image/jpeg",
-                    size: image.size,
-                    encoding: "7bit",
-                    destination: 'image/',
-                    filename: image.originalname,
-                    originalname: image.originalname
-                // },
-            },
-            headers: {
-                Authorization: `Bearer ${token}`,
-                // "Content-Type": "multipart/form-data"
-            }
-        })
-        .then (res => {
-            console.log(res)
-        })
-    }
-
-    const newHandleSubmit = (event) => {
-        event.preventDefault()
-
-        const myForm = document.getElementById("test")
-        formData = new FormData(myForm)
-
-        formData.append('title', imagePostData.title)
-        formData.append('category', imagePostData.category);
-        // formData.append('image', myForm)
-
-        // let fd = new FormData(image);
-        // fd.append('image', image);
-
-        axios({
-            url: 'http://localhost:4000/posts/create/image',
-            method: 'POST',
-            // enctype: 'multipart/form-data',
-            data: formData,
-            headers: {
-                Authorization: `Bearer ${token}`,
-                // "Content-Type": "multipart/form-data"
-            }
-        })
-        .then (res => {
-            console.log(res)
-        })
-    }
-
-    const newHandleImageUpload = (event) => {
-        const myForm = document.getElementById("test")
-        formData = new FormData(myForm)
-
-        // axios({
-        //     url: 'http://localhost:4000/posts/create/single/image',
-        //     method: 'POST',
-        //     // enctype: 'multipart/form-data',
-        //     data: formData,
-        //     headers: {
-        //         Authorization: `Bearer ${token}`,
-        //         "Content-Type": "multipart/form-data"
-        //     }
-        // })
-        // .then (res => {
-        //     console.log(res)
-        // })
-    }
+    
 
     return (
 			<div className='ImagePost'>
@@ -174,18 +61,18 @@ __proto__: Object */
 				<Category handlePostDataInput={handlePostDataInput} />
 
 				<Row className='PostOptionsRow'>
-					<SaveDraft saveDraftPost={saveDraftPost} />
-					<PostButton post={post} onSubmit={onSubmit} newHandleSubmit={newHandleSubmit} />
-
-					<form id='test' action='/upload' enctype='multipart/form-data'>
+					<form id='imageForm' action='/upload' enctype='multipart/form-data'>
 						<input
 							type='file'
 							id='file'
 							accept='.jpg'
 							name='image'
-							/* onChange={newHandleImageUpload} */
 						/>
 					</form>
+                        
+					<SaveDraft saveDraftPost={saveDraftPost} />
+					<PostButton imagePost={imagePost} />
+
 				</Row>
 			</div>
 		);
