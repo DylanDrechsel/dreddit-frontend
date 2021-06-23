2c8783b7fbfe8d4941d157a5c1825505a92edf89
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-23 13:17:59 -0400

    Added GetPosts.js and started working with components

--- src/UserPage/UserChoiceBar.js
+++ src/UserPage/UserChoiceBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap'
+import GetPosts from './UserChoiceBarComponents/GetPosts'
 
 const UserChoiceBar = () => {
     return (
-        <div className="UserChoiceBar">
-            <h1 style={{ color: 'white' }}>Hello</h1>
-        </div>
-    );
+			<div className='UserChoiceBar'>
+                <GetPosts />
+			</div>
+		);
 };
 
 export default UserChoiceBar;
---
+++ src/UserPage/UserChoiceBarComponents/GetPosts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetPosts = () => {
+    return (
+        <div className="UserPostButton">
+            <h5 style={{color: 'white'}}>Posts</h5>
+        </div>
+    );
+};
+
+export default GetPosts;
