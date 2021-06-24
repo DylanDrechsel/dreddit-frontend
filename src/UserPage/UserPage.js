3cf2dd9cda3b8ae9636e7fde27d10af496b5109f
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-24 12:20:11 -0400

    Added PublishedPost.js and imported it to UserPage.js. Also created the axios request to get the users Published Post

--- src/UserPage/UserPage.js
+++ src/UserPage/UserPage.js
@@ -1,19 +1,26 @@
 import React, { useEffect } from 'react';
-import { websiteState } from '../App';
+import { websiteState, tokenState } from '../App';
 import { useRecoilState } from 'recoil';
 import './UserPage.css';
 import UserChoiceBar from './UserChoiceBar';
+import axios from 'axios';
+import PublishedPost from './PublishedPost';
 
 const UserPage = () => {
     const [website, setWebsite] = useRecoilState(websiteState);
+    const [token] = useRecoilState(tokenState)
 
 	useEffect(() => {
-		setWebsite('userPage');
+		setWebsite('userPage/posts');
 	}, []);
+
+    
 
     return (
         <div>
             <UserChoiceBar />
+
+            {website === 'userPage/posts' ? (<PublishedPost />) : null}
         </div>
     );
 };
