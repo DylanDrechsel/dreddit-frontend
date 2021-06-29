4419336bd17bbc52246630b32e0d272149287560
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-29 16:29:18 -0400

    imported post data to edit modal so the user can see what they are editing

--- src/SubmitPage/PostBoxes/Components/Title.js
+++ src/SubmitPage/PostBoxes/Components/Title.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Form } from 'react-bootstrap'
 
-const Title = ({ handlePostDataInput }) => {
+const Title = ({ handlePostDataInput, postTitle }) => {
     return (
         <div className="TitleBar">
             <Form>
                 <Form.Group controlId="title">
-                    <Form.Control type="title" placeholder="Title" style={{ backgroundColor: 'black', color: 'white', border: '1px solid grey'}} onChange={handlePostDataInput}/>
+                    <Form.Control type="title" placeholder={postTitle ? postTitle : "Title"} style={{ backgroundColor: 'black', color: 'white', border: '1px solid grey'}} onChange={handlePostDataInput}/>
                 </Form.Group>
             </Form>
         </div>
