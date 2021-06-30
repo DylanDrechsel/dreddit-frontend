60f991bb55aaf0c2707d3637854665007be5f55d
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-30 10:36:30 -0400

    Completed EditPostModal for text post

--- src/SubmitPage/PostBoxes/Components/Category.js
+++ src/SubmitPage/PostBoxes/Components/Category.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
-const Category = ({ handlePostDataInput, postCategory }) => {
+const Category = ({ handlePostDataInput, postCategory, handleDataChange }) => {
     return (
         <div className="CategoryBar">
             <Form>
                 <Form.Group controlId="category">
-                    <Form.Control type="category" defaultValue={postCategory} placeholder={"Category"} style={{ backgroundColor: 'black', color: 'white', border: '1px solid grey'}} onChange={handlePostDataInput}/>
+                    <Form.Control type="category" defaultValue={postCategory} placeholder={"Category"} style={{ backgroundColor: 'black', color: 'white', border: '1px solid grey'}} onChange={handlePostDataInput ? handlePostDataInput : handleDataChange}/>
                 </Form.Group>
             </Form>
         </div>
