6e5569c225f5d66a223886ba1050a049a1bd3c86
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-30 15:26:12 -0400

    css fixes and imported SignUpModal to SignUp.js

---
+++ src/LandingPage/SignInSignUp/Components/SignUpModal.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { tokenState, userIdState, userNameState } from '../../../App';
+import { useRecoilState } from 'recoil';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+const SignUpModal = () => {
+    const [token, setToken] = useRecoilState(tokenState);
+    const [userId, setUserId] = useRecoilState(userIdState)
+    const [userName, setUserName] = useRecoilState(userNameState)
+    const [userData, setUserData] = useState({
+		email: null,
+		password: null,
+	});
+
+    return (
+        <div>
+            HELLO
+        </div>
+    );
+};
+
+export default SignUpModal;
