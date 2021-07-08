1f7a44bc1e1921cc954394d13df9facf5e4d7680
DylanDrechsel <DrechselDylan@gmail.com>
2021-07-08 13:12:33 -0400

    Added Content to PostComments.js and tested it

--- src/PostDetailPage/PostDetailFeed/CommentFeed/Components/PostComment.js
+++ src/PostDetailPage/PostDetailFeed/CommentFeed/Components/PostComment.js
@@ -2,14 +2,36 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { tokenState, userNameState } from '../../../../App';
 import { useRecoilState } from 'recoil';
+import Content from '../../../../SubmitPage/PostBoxes/Components/Content'
 
 const PostComment = ({ id }) => {
     const [token] = useRecoilState(tokenState);
     const [userName] = useRecoilState(userNameState);
+    const [commentData, setCommentData] = useState()
+
+    const handleCommentData = (event) => {
+		const input = { ...commentData };
+		input[event.target.id] = event.target.value;
+		setCommentData(input);
+	};
+
+    const publishedComment = () => {
+        axios.get({
+            url: `http://localhost:4000/comments/create/${id}`,
+            method: 'POST',
+            headers: {
+					Authorization: `Bearer ${token}`,
+			}
+        })
+        .then(res => console.log(res))
+    }
+
+    console.log(commentData)
 
     return (
         <div className="PostComment">
             <p style={{ fontSize: '12px'}}>Comment as {userName}</p>
+            <Content handleCommentData={handleCommentData}/>
         </div>
     );
 };
--- src/SubmitPage/PostBoxes/Components/Content.js
+++ src/SubmitPage/PostBoxes/Components/Content.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Form, FormControl } from 'react-bootstrap'
 
-const Content = ({ handlePostDataInput, postContent, handleDataChange }) => {
+const Content = ({ handlePostDataInput, postContent, handleDataChange, handleCommentData }) => {
     return (
 			<div className="ContentBar">
                 <Form.Group controlId="content">
-				    <FormControl as='textarea' defaultValue={postContent} placeholder={"Post"} style={{ backgroundColor: 'black', color: 'white', border: '1px solid grey'}} onChange={handlePostDataInput ? handlePostDataInput : handleDataChange} />
+				    <FormControl as='textarea' defaultValue={postContent} placeholder={"Post"} style={{ backgroundColor: 'black', color: 'white', border: '1px solid grey'}} onChange={handlePostDataInput ? handlePostDataInput : handleCommentData ? handleCommentData : handleDataChange} />
                 </Form.Group>
 			</div>
 		);
